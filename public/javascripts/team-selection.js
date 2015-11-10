4fb5cc718b5b0247d4ec91861c477eb44f6d6ff2
Luke Duncan <luke.will.duncan@gmail.com>
2015-11-10 10:21:26 -0600

    array with selected teams configured

--- public/javascripts/team-selection.js
+++ public/javascripts/team-selection.js
@@ -1,17 +1,24 @@
 $(function() {
 
 	var count = 0;
-	console.log(count);
 	
 	var predictbutton = $('<button class="predict-button">Predict Now</button>');
 	var refreshbutton = $('<button class="refresh-button">Refresh Teams</button>');
 
+	var selectedTeams = new Array();
+
+
+
+	
 	$('.select-teams').on('click', function(){
 
+		
+		// if two teams have been selected, function ends
 		if (count > 1) {
 			return
 		}
 
+		// if no team has been selected, put this first team into .team1 div
 		else if (count === 0) {	
 				$(this).parent().removeClass("team").addClass("two-teams");
 				$(this).parent().removeClass("team-name").addClass("team-name-chosen");
@@ -19,12 +26,22 @@ $(function() {
         $(".versus").html('<h1>vs</h1>');
         $(this).remove();
 
+			 $('.team-name-chosen').each(function(){
+					selectedTeams.push($(this).text());
+				});
+
         count++;
     } 
+    // if one team has been selected, input next team into .team2 div
     else {
 				$(this).parent().removeClass('team').addClass("two-teams");
+				$(this).parent().removeClass("team-name").addClass("team-name-chosen");
 				$(this).parent().appendTo(".team2");
 				$(this).remove();
+
+				var team2 = $('.team-name-chosen').each(function(){
+					selectedTeams.push($(this).text());
+				});
 
 				count++;
 
@@ -33,12 +50,16 @@ $(function() {
     }
 	});
 
+	
+	// sends two teams into brain
 	$(predictbutton).on('click', function(){
-		var value = $('.team-name-chosen h4').html();
-		console.log(value, "grabbing team-name");
+		var teamString = selectedTeams.toString();
+		console.log(selectedTeams);
+		console.log(teamString);
 
 	})
 
+	// refreshes page in case someone wants to select other teams
 	$(refreshbutton).on('click', function(){
 		location.reload();
 	})
