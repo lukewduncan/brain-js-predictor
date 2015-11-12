ece62e0d715ff1d1089cedf829062ebce1ed6f42
Luke Duncan <luke.will.duncan@gmail.com>
2015-11-12 15:42:45 -0600

    commenting out models

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var Player = require('../models/player');
-var Team = require('../models/team');
+// var Player = require('../models/player');
+// var Team = require('../models/team');
 var brain = require('brain');
 
 
